49f1e3edc3e21e435be2278fdc596d09a05789cd
mj <will@ponsonby.org>
2021-08-10 04:51:48 +0100

    local updates

--- links/shs/brightness.sh
+++ links/shs/brightness.sh
@@ -1,5 +1,1 @@
-xrandr --output DisplayPort-0 --brightness $1
-xrandr --output DisplayPort-1 --brightness $1
-xrandr --output DisplayPort-2 --brightness $1
-xrandr --output HDMI-A-0 --brightness $1
-xrandr --output HDMI-A-1 --brightness $1
+xrandr --output eDP-1 --brightness $1
