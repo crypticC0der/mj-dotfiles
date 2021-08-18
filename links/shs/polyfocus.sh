481521a8c45e2a552d8315315b240f2e5ad7c496
mj <will@ponsonby.org>
2021-08-18 14:32:48 +0100

    laptop specific updates

--- links/shs/polyfocus.sh
+++
@@ -1,5 +0,0 @@
-if [ -n "$(xprop -root | grep '_NET_CURRENT_DESKTOP(CARDINAL) = 4')" ];then
-	xprop -f _NET_WM_WINDOW_OPACITY 32c -set _NET_WM_WINDOW_OPACITY 0xffffffff -name "polybar_main"	
-else
-	xprop -f _NET_WM_WINDOW_OPACITY 32c -set _NET_WM_WINDOW_OPACITY 0xcccccccc -name "polybar_main"	
-fi
