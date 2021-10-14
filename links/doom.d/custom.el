5db91459233c706970498e69d90dc856d2cb7558
crypticC0der <will@ponsonby.org>
2021-10-14 16:27:05 +0100

    custom

---
+++ links/doom.d/custom.el
@@ -0,0 +1,16 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(doom-modeline-buffer-encoding t)
+ '(doom-modeline-indent-info t)
+ '(doom-modeline-major-mode-icon t)
+ '(package-selected-packages
+   '(slack alert async ghub powerline-evil jammer no-spam hardcore-mode impatient-mode powerline markdown-preview-mode)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
